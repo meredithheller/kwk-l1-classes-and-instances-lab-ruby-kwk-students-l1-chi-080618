f2d866bf5e9267c6eb8becf6615c7d483a68e271
meredithheller <meredithheller@outlook.com>
2018-08-10 16:29:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
-  def initialize(fido, snoopy, lassie)
-    @fido = fido
-    @snoopy = snoopy
-    @lassie = lassie
+  def initialize(dog)
+    @dog = dog
   end
 end
+
+snoopy = Dog.new
